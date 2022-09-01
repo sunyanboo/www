0aa9c45db6539f8ada388d106d39c6c061c85e9d
sunyanboo <112537645+sunyanboo@users.noreply.github.com>
2022-09-01 13:50:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -101,15 +101,19 @@ def get_birthday(birthday, year, today):
         birth_day = str(birth_date.__sub__(today)).split(" ")[0]
     return birth_day
  
- 
-def get_ciba():
+ def get_daily_love():
+    #每日一句情话
+    url = "https://api.lovelive.tools/api/SweetNothings/Serialization/Json"
+    r = requests.get(url)
+    all_dict = json.loads(r.text)
+    sentence = all_dict['returnObj'][0]
+    daily_love = sentence
+    return daily_love
+   
+ def get_ciba():
+    #每日一句英语（来自爱词霸）
     url = "http://open.iciba.com/dsapi/"
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    r = get(url, headers=headers)
+    r = get(url)
     note_en = r.json()["content"]
     note_ch = r.json()["note"]
     return note_ch, note_en
